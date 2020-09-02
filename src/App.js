39f7fede8f866feb1c97919a00cf6994961bfb24
yehuda8587 <nurielbiz@gmail.com>
2020-09-02 15:44:55 -0400

    teting

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import CardList from './CardList';
+import Scroll from './Scroll';
+import SearchBox from './SearchBox';
+import './App.css';
+
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+        robots:[],
+        searchfield: ''
+    }
+
+}
+
+componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }))
+}
+
+onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value}) 
+}
+
+    render() {
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+            return !robots.length ?
+                <h1>Loading</h1>: 
+                (
+            <div className='tc'>
+                <h1 className="f1">Soccer Friends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                <CardList robots={filteredRobots} />
+                </Scroll>
+            </div>
+            );
+        } 
+    }
+
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Polina',
+        username: 'polly',
+        email: 'polina@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'udi',
+        username: 'juda',
+        email: 'juda@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Guy',
+        username: 'Feissmann',
+        email: 'guyrealty@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Vanessa',
+        username: 'VanessaTheActress',
+        email: 'Vanessa@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Ben',
+        username: 'Benjamin',
+        email: 'Benjamin@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Amitay',
+        username: 'AmitayPinkEyes',
+        email: 'Amitay@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Yehiel',
+        username: 'YemanMAN',
+        email: 'Yehi@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Aviel',
+        username: 'Avielooo',
+        email: 'Aviel@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Hanan',
+        username: 'Hananovich',
+        email: 'waltershanan@gmail.com'
+    }
+]
